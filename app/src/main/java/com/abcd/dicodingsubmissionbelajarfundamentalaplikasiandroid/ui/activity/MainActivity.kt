51c08b618cc403ed848b848e13d1e812877983bf
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-08-26 03:24:35 +0800

    melakukan update pada main activity

--- app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/activity/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.R
 import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
