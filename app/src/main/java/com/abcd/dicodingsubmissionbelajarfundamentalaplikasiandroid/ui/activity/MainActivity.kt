2ca68523b7b0bb15e4944c74bb365608298c64e5
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-08-26 20:28:09 +0800

    Mengubah nama parameter fragment

--- app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/activity/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.R
 import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.databinding.ActivityMainBinding
+import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.ui.fragment.dashboard.UpcomingFragment
+import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.ui.fragment.home.HomeFragment
+import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.ui.fragment.notifications.FinishedFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,18 +24,44 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_activity_main, HomeFragment())
+            .commit()
 
-        val navView: BottomNavigationView = binding.navView
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    setHomeFragment()
+                    true
+                }
+                R.id.navigation_upcoming -> {
+                    setUpcomingFragment()
+                    true
+                }
+                R.id.navigation_finished -> {
+                    setFinishedFragment()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_upcoming, R.id.navigation_finished
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    private fun setHomeFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_activity_main, HomeFragment())
+            .commit()
+    }
+
+    fun setUpcomingFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_activity_main, UpcomingFragment())
+            .commit()
+    }
+
+    fun setFinishedFragment(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment_activity_main, FinishedFragment())
+            .commit()
     }
 }
