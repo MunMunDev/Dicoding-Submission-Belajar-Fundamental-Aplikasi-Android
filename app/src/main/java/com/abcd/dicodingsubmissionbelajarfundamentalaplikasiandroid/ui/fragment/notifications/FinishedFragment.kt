6b20f43a40faded346bbbcdbfbe7bb69c08f1a43
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-08-26 20:26:51 +0800

    mengganti nama fragment

--- app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/fragment/notifications/FinishedFragment.kt
+++ app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/fragment/notifications/FinishedFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.databinding.FragmentNotificationsBinding
+import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.databinding.FragmentFinishedBinding
 
 class FinishedFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFinishedBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +25,9 @@ class FinishedFragment : Fragment() {
         val notificationsViewModel =
             ViewModelProvider(this).get(FinishedViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _binding = FragmentFinishedBinding.inflate(inflater, container, false)
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
