531f42936a16a72ce778d02c7b259543476089ee
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-09-05 22:19:54 +0800

    menambahkan setting untuk dark mode. dan menambahkan untuk mengaktifkan reminder event, tapi belum dapat digunakan.

--- app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/fragment/settings/SettingsFragment.kt
+++ app/src/main/java/com/abcd/dicodingsubmissionbelajarfundamentalaplikasiandroid/ui/fragment/settings/SettingsFragment.kt
@@ -1,31 +1,71 @@
 package com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.ui.fragment.settings
 
-import androidx.fragment.app.viewModels
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.CheckBoxPreference
+import androidx.preference.PreferenceFragmentCompat
 import com.abcd.dicodingsubmissionbelajarfundamentalaplikasiandroid.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private lateinit var DARK_MODE: String
+    private lateinit var REMINDER: String
+
+    private lateinit var darkModePreferences: CheckBoxPreference
+    private lateinit var reminderPreferences: CheckBoxPreference
 
     companion object {
         fun newInstance() = SettingsFragment()
     }
 
-    private val viewModel: SettingsViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+        init()
+        setSummaries()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    private fun init() {
+        DARK_MODE = resources.getString(R.string.key_theme)
+        REMINDER = resources.getString(R.string.key_reminder)
+
+        darkModePreferences = findPreference<CheckBoxPreference>(DARK_MODE)!!
+        reminderPreferences = findPreference<CheckBoxPreference>(REMINDER)!!
+    }
+
+    private fun setSummaries() {
+        val sh = preferenceManager.sharedPreferences
+        darkModePreferences.isChecked = sh.getBoolean(DARK_MODE, false)
+        reminderPreferences.isChecked = sh.getBoolean(REMINDER, false)
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        when (key) {
+            DARK_MODE -> {
+                val enabled = sharedPreferences?.getBoolean(DARK_MODE, false) ?: false
+                darkModePreferences.isChecked = enabled
+
+                if(enabled)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+            }
+            REMINDER -> {
+                val enabled = sharedPreferences?.getBoolean(REMINDER, false) ?: false
+                reminderPreferences.isChecked = enabled
+            }
+        }
     }
 }
